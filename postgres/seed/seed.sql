4f145a0e001e4c473f835dfe82a0de80b0a6215c
Prem Acharya <prem.acharya@hotmail.com>
2018-10-02 09:23:55 +1000

    JWT added

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,5 +1,5 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (name, email, entries, joined) values('p', 'p@p.com', 5, '2018-09-09');
+INSERT INTO users (name, email, entries, joined) values('a', 'a@a.com', 5, '2018-09-09');
 INSERT INTO login (hash, email) values('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'a@a.com');
 COMMIT;
